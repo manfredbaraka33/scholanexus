e299f41606274f227a84a54359003484e20d42de
Manfred Baraka Josephat <163660029+manfredbaraka33@users.noreply.github.com>
2026-05-30 02:15:07 +0300

    Update admin.py

--- backend/app/api/v1/endpoints/admin.py
+++ backend/app/api/v1/endpoints/admin.py
@@ -7,6 +7,7 @@ from fastapi.responses import StreamingResponse
 from sqlalchemy.orm import Session
 from pydantic import BaseModel, Field
 from sqlalchemy.orm.attributes import flag_modified
+from datetime import datetime
 
 from app.core.database import get_db
 from app.core.deps import require_admin, require_teacher
